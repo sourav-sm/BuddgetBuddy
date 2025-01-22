29e96ee9fa1727c2e76e1eb13b0eed8d234ef6b9
Sourav <souravmohanta4428@gmail.com>
2025-01-23 03:18:47 +0530

    clenning

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,16 +1,3 @@
-// import * as dotenv from 'dotenv';
-// dotenv.config({ path: '.env.local' });
-
-// /**@type {import("drizzle-kit").Config} */
-// export default {
-//     schema:"./utils/schema.jsx",
-//     dialect:'postgresql',
-//     dbCredentials:{
-//         url:process.env.DATABASE_URL,
-//     },
-// };
-
-
 /**@type {import("drizzle-kit").Config} */
 export default {
     schema:"./utils/schema.jsx",
