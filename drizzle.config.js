39b20e0f066205ce3c8565fffd8c06e9677d9948
SOURAV MOHANTA <116539402+sourav-sm@users.noreply.github.com>
2025-02-21 14:46:45 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,7 @@ export default {
     schema:"./utils/schema.js",
     dialect:'postgresql',
     dbCredentials:{
-        url:process.env.DATABASE_URL,
+        // url:process.env.DATABASE_URL,
+        url:"postgresql://developersourav135:GDWbeuk6ZPv0@ep-royal-bread-48297123.us-east-2.aws.neon.tech/Expense-Tracker?sslmode=require"
     },
 };
